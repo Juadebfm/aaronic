86c64f5af642e511cc166272fc3e401bbabbcbce
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-06-01 20:08:38 +0100

    fix

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,10 +1,11 @@
 // src/components/Loading.js
 import React from "react";
+import Aaronic from "../assets/fulllogo.png";
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-      <p>Loading...</p>
+    <div className="fixed inset-0 flex flex-col md:flex-row items-center justify-center bg-white z-50">
+      <img src={Aaronic} alt="LOGO" className="w-[200px] h-[50px] animate-bounce" />
     </div>
   );
 };
