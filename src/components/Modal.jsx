c6e5982cbb4269b1d5e2fa407accf1a5313fdda4
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-06-09 08:14:26 +0100

    fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,6 +14,7 @@ const Modal = ({ isOpen, onClose }) => {
 
   const [formErrors, setFormErrors] = useState({});
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -40,6 +41,7 @@ const Modal = ({ isOpen, onClose }) => {
     if (Object.keys(errors).length > 0) {
       setFormErrors(errors);
     } else {
+      setIsSubmitting(true);
       submitForm();
       setFormErrors({});
     }
@@ -63,6 +65,9 @@ const Modal = ({ isOpen, onClose }) => {
       })
       .catch((error) => {
         console.error("Error occurred while submitting form:", error);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
       });
   };
 
@@ -77,140 +82,153 @@ const Modal = ({ isOpen, onClose }) => {
       <div className="bg-white rounded-md shadow-lg z-10 w-full lg:w-[30%] max-h-[70vh] overflow-y-auto py-14 relative px-[30px] lg:px-14">
         <button
           onClick={onClose}
-          className="mt-4 p-4 bg-myYellow rounded-full text-black absolute right-5 lg:right-10 top-5 hover:shadow-lg duration-150 ease-linear shadow-black"
+          className="mt-4 p-2 bg-myYellow rounded-full text-black absolute right-5 lg:right-5 top-0 hover:shadow-lg duration-150 ease-linear shadow-black"
         >
           <RiCloseLargeLine className="text-base lg:text-2xl text-white" />
         </button>
-
-        {!formSubmitted && (
-          <h2 className="text-3xl lg:text-[40px] font-bold text-start text-myYellow my-8">
-            Get Started
-          </h2>
-        )}
 
         {formSubmitted ? (
           <div className="text-center text-lg font-semibold text-green-600">
             Thanks! We will respond to you shortly.
           </div>
         ) : (
-          <form className="space-y-7" onSubmit={handleSubmit}>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="businessName" className="text-[15px] mb-2">
-                Business Name
-              </label>
-              <input
-                type="text"
-                name="businessName"
-                value={formData.businessName}
-                onChange={handleChange}
-                placeholder="Enter your business name"
-                className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
-                  formErrors.businessName ? "border-red-500" : "border-black"
-                }`}
-              />
-              {formErrors.businessName && (
-                <span className="text-red-500">{formErrors.businessName}</span>
-              )}
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="industry" className="text-[15px] mb-2">
-                Industry
-              </label>
-              <input
-                type="text"
-                name="industry"
-                value={formData.industry}
-                onChange={handleChange}
-                placeholder="Enter Industry"
-                className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
-                  formErrors.industry ? "border-red-500" : "border-black"
-                }`}
-              />
-              {formErrors.industry && (
-                <span className="text-red-500">{formErrors.industry}</span>
-              )}
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="email" className="text-[15px] mb-2">
-                Email *
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                placeholder="Enter your email"
-                className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
-                  formErrors.email ? "border-red-500" : "border-black"
-                }`}
-              />
-              {formErrors.email && (
-                <span className="text-red-500">{formErrors.email}</span>
-              )}
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="phoneNumber" className="text-[15px] mb-2">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="phoneNumber"
-                value={formData.phoneNumber}
-                onChange={handleChange}
-                placeholder="Enter Phone Number"
-                className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
-                  formErrors.phoneNumber ? "border-red-500" : "border-black"
-                }`}
-              />
-              {formErrors.phoneNumber && (
-                <span className="text-red-500">{formErrors.phoneNumber}</span>
-              )}
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="services" className="text-[15px] mb-2">
-                Services You Want
-              </label>
-              <select
-                name="services"
-                value={formData.services}
-                onChange={handleChange}
-                className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
-                  formErrors.services ? "border-red-500" : "border-black"
-                }`}
-              >
-                <option value="">Select Services</option>
-                <option value="Oil Trading">Oil Trading</option>
-                <option value="Shipping">Shipping</option>
-                <option value="Marine Logistics">Marine Logistics</option>
-                <option value="Cargo Handling">Cargo Handling</option>
-                <option value="Oilfield Services">Oilfield Services</option>
-              </select>
-              {formErrors.services && (
-                <span className="text-red-500">{formErrors.services}</span>
-              )}
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <label htmlFor="moreInfo" className="text-[15px] mb-2">
-                Tell us more about your company
-              </label>
-              <textarea
-                name="moreInfo"
-                value={formData.moreInfo}
-                onChange={handleChange}
-                placeholder="Leave additional messages"
-                className="placeholder:text-gray-500 placeholder:capitalize font-extralight border border-black px-8 py-4 rounded-lg w-full mb-8"
-              />
-            </div>
-            <div className="flex items-center justify-center">
-              <Button
-                btnText={"Submit"}
-                className={
-                  "bg-myYellow text-black w-full lg:w-[80%] py-4 mx-auto"
-                }
-              />
-            </div>
-          </form>
+          <>
+            <h2 className="text-3xl lg:text-[40px] font-bold text-start text-myYellow my-8">
+              Get Started
+            </h2>
+            <form className="space-y-7" onSubmit={handleSubmit}>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="businessName" className="text-[15px] mb-2">
+                  Business Name
+                </label>
+                <input
+                  type="text"
+                  name="businessName"
+                  value={formData.businessName}
+                  onChange={handleChange}
+                  placeholder="Enter your business name"
+                  className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
+                    formErrors.businessName ? "border-red-500" : "border-black"
+                  }`}
+                />
+                {formErrors.businessName && (
+                  <span className="text-red-500">
+                    {formErrors.businessName}
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="industry" className="text-[15px] mb-2">
+                  Industry
+                </label>
+                <select
+                  name="industry"
+                  value={formData.industry}
+                  onChange={handleChange}
+                  className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
+                    formErrors.industry ? "border-red-500" : "border-black"
+                  }`}
+                >
+                  <option value="">Select Industry</option>
+                  <option value="Transport">Transportation</option>
+                  <option value="Food">Food and Beverage</option>
+                  <option value="Healthcare">Healthcare</option>
+                  <option value="Retail">Retail</option>
+                  <option value="Manufacturing">Manufacturing</option>
+                  <option value="Education">Education</option>
+                  <option value="Technology">Technology</option>
+                  <option value="Financial Services">Financial Services</option>
+                  <option value="Construction">Construction</option>
+                  <option value="Hospitality and Tourism">
+                    Hospitality and Tourism
+                  </option>
+                  <option value="Other">Other</option>
+                </select>
+                {formErrors.industry && (
+                  <span className="text-red-500">{formErrors.industry}</span>
+                )}
+              </div>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="email" className="text-[15px] mb-2">
+                  Email *
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  placeholder="Enter your email"
+                  className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
+                    formErrors.email ? "border-red-500" : "border-black"
+                  }`}
+                />
+                {formErrors.email && (
+                  <span className="text-red-500">{formErrors.email}</span>
+                )}
+              </div>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="phoneNumber" className="text-[15px] mb-2">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                  placeholder="Enter Phone Number"
+                  className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
+                    formErrors.phoneNumber ? "border-red-500" : "border-black"
+                  }`}
+                />
+                {formErrors.phoneNumber && (
+                  <span className="text-red-500">{formErrors.phoneNumber}</span>
+                )}
+              </div>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="services" className="text-[15px] mb-2">
+                  Services You Want
+                </label>
+                <select
+                  name="services"
+                  value={formData.services}
+                  onChange={handleChange}
+                  className={`placeholder:text-gray-500 placeholder:capitalize font-extralight border px-8 py-4 rounded-lg w-full ${
+                    formErrors.services ? "border-red-500" : "border-black"
+                  }`}
+                >
+                  <option value="">Select Services</option>
+                  <option value="Oil Trading">Oil Trading</option>
+                  <option value="Shipping">Shipping</option>
+                  <option value="Marine Logistics">Marine Logistics</option>
+                  <option value="Cargo Handling">Cargo Handling</option>
+                </select>
+                {formErrors.services && (
+                  <span className="text-red-500">{formErrors.services}</span>
+                )}
+              </div>
+              <div className="flex flex-col items-start justify-start w-full">
+                <label htmlFor="moreInfo" className="text-[15px] mb-2">
+                  Tell us more about your company
+                </label>
+                <textarea
+                  name="moreInfo"
+                  value={formData.moreInfo}
+                  onChange={handleChange}
+                  placeholder="Leave additional messages"
+                  className="placeholder:text-gray-500 placeholder:capitalize font-extralight border border-black px-8 py-4 rounded-lg w-full mb-8"
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <Button
+                  btnText={isSubmitting ? "Submitting..." : "Submit"}
+                  className={
+                    "bg-myYellow text-black w-full lg:w-[80%] py-4 mx-auto"
+                  }
+                />
+              </div>
+            </form>
+          </>
         )}
       </div>
     </div>
