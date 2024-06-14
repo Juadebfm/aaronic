883666367bab824b54939475059396cb69ae1e13
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-06-14 11:05:46 +0100

    fix

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,6 +3,7 @@ import Partner1 from "../assets/restoration.webp";
 import Partner2 from "../assets/empisto_partner.png";
 import Partner3 from "../assets/crystalline_partners.png";
 import Partner4 from "../assets/milan_partners.png";
+import Partner5 from "../assets/bluespring.webp";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -13,7 +14,7 @@ const Partners = () => {
   return (
     <div className="flex items-center justify-center flex-col px-[25px] lg:px-28 py-10 lg:py-20 gap-10">
       <h2 className="text-[24px] font-bold">Our Partners</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-4 place-items-center gap-10">
+      <div className="grid grid-cols-1 lg:grid-cols-5 place-items-center gap-10">
         <img
           src={Partner1}
           alt="Restoration"
@@ -40,6 +41,13 @@ const Partners = () => {
           src={Partner4}
           alt="Milan"
           className="h-[84px] w-[238px]"
+          data-aos="fade-left"
+          data-aos-duration="2000"
+        />
+        <img
+          src={Partner5}
+          alt="Milan"
+          className="h-[84px] w-auto"
           data-aos="fade-left"
           data-aos-duration="2000"
         />
