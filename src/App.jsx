1154518361d3f492ca1a9e317fe7b3f2dba6ebf0
Julius Adebowale <juadebgabriel@gmail.com>
2024-10-20 15:35:19 +0100

    feat: created a build folder

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const PageRoutes = ({ setLoading }) => {
     setLoading(true);
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3200); // Simulating loading time, adjust as needed
+    }, 3500); // Simulating loading time, adjust as needed
 
     return () => clearTimeout(timer);
   }, [location, setLoading]);
